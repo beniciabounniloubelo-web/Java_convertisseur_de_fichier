9c255299db30545acd50be144b7985639c9ceefa
bounnilo <benicia.bounni-loubelo@etu.u-pec.fr>
2026-01-06 04:42:05 +0100

    Corrections canonique

--- src/rootfolder/canonique/TabCaFinale.java
+++ src/rootfolder/canonique/TabCaFinale.java
@@ -1,22 +1,25 @@
 package canonique;
 
-//creation de la table canonique finale
+import java.util.Map;
+import java.util.TreeMap;
+import java.util.Queue;
 
+//creation de la table canonique finale
 public class TabCaFinale {
 
-    public static void createTabCaFinale(String code, Queue<Entree> afile, Map<Integer, String> tabCanonique) {
+    public static void createTabCaFinale(String code, Queue<TabCaEntree> afile, Map<Integer, String> tabCanonique) {
 
         if (afile.isEmpty()) { //la fonction tourne jusqu'a la file soit vide - condition d'arret
             return;
         }
 
-        Entree e1 = afile.poll(); //on a une entree
+        TabCaEntree e1 = afile.poll(); //on a une entree
         String newcode = code; //on recupere le code
         while (newcode.length() < e1.getLongueurCode()) { //comparaison
         newcode = newcode + "0"; //on donne au code la longueur souhaite
         }
 
-        if (newcode.length() == e1.getLongueurCode){ //si code a la bonne longueur
+        if (newcode.length() == e1.getLongueurCode()){ //si code a la bonne longueur
             tabCanonique.put(e1.getValeur(), newcode); //ajout dans le dico
         }
 
